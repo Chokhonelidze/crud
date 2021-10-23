9613272c1b5555309cd6be494766bde8ac561874
George Chokhonelidze <g_chokhonelidze@outlook.com>
2021-10-23 08:03:20 -0500

    11.22.21

--- server.js
+++ server.js
@@ -1,64 +1,71 @@
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb://admin:cst10b002944@localhost:27017/mydb?retryWrites=true&w=majority";
+
+var client = new MongoClient(uri);
+
+try {
+  client.connect();
+  var con = client.db("mydb");
+  console.log("db is connected");
+} catch (err) {
+  console.log(err);
+}
+
 const express = require("express");
+const { response, request } = require("express");
 const app = express();
 app.use(express.json());
+
 //app.use( express.urlencoded({extended:true}))
 const PORT = process.env.PORT || 3000;
-const data = [
-  {
-    id: 1,
-    address: "199 no address street",
-    owner: "George",
-    cars: [
-      {
-        brand: "Honda",
-        model: "fit",
-        year: "2009",
-        color: "grey",
-      },
-      {
-        brand: "Tesla",
-        model: "Y",
-        year: "2021",
-        color: "blue",
-      },
-    ],
-  },
-];
+
 //READ
 app.get("/", (req, res) => {
-  res.send(data);
+  con
+    .collection("cars")
+    .find({})
+    .toArray((err, result) => {
+      if (err) {
+        res.sendStatus(400);
+      } else {
+        res.send(result).status(200);
+      }
+    });
 });
 //create
 app.post("/", (req, res) => {
-  req.body.id = data.length+1;
-  data.push(req.body);
+  con.collection("cars").insertOne(req.body);
   res.sendStatus(200);
 });
 //DELETE
 app.delete("/", (req, res) => {
 
   let id = req.body.id;
-  for (let i in data) {
-    if (data[i].id == id) {
-      delete(data[i]);
-      res.sendStatus(200);
-      return true;
+  con.collection("cars").findOneAndDelete({ id: id },(err,response)=>{
+    if(err){
+      res.sendStatus(400);
     }
-  }
-  res.sendStatus(400);
-
+    else{
+      res.sendStatus(200);
+    }
+  });
 });
 //UPDATE
 app.put("/", (req, res) => {
-    let id = req.body.id;
-    for (let i in data) {
-        if (data[i].id == id) {
-          data[i] = req.body;
-          res.sendStatus(200);
-          return true;
-        }
-      }
+  let id = {'id':req.body.id};
+  let update = {$set:req.body};
+  con.collection('cars').updateOne(id,update,(err,response)=>{
+    if(err){
       res.sendStatus(400);
+      throw err;
+    }
+    else{
+      res.sendStatus(200);
+    }
+  })
+  
 });
 
 app.listen(PORT, () => {
