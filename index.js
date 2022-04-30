0fd226daca3b366cc9c121990312e8fb5d26fd43
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-04-30 09:02:47 -0500

    fix:

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ const mongoose = require("mongoose");
 
 mongoose.connect(DB, { useNewUrlParser: true }).then(() => {
   const app = express();
+  const corsWhitelist = [
+    'http://localhost:5000',
+    'http://localhost:5001',
+     process.env.SERVER
+    ];
 
   let headers = (req, res, next) => {
      // Website you wish to allow to connect
     //res.setHeader("Access-Control-Allow-Origin", "https://chokhonelidze.github.io/,http://localhost:5000,http://myhome.smho.site:5000");
-    res.setHeader("Access-Control-Allow-Origin", SERVER);
+    if(corsWhitelist.indexOf(req.headers.origin) !== -1) {
+      res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+    }
 
     // Request methods you wish to allow
     res.header(
